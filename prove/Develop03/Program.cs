a5ad05abe5ed980ad0b7879dfa4eb60226d6a681
Marcos Guimarães <99098413+Gui1986@users.noreply.github.com>
2024-03-09 01:55:07 -0300

    Assing. Conclusion - Developer—Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -85,7 +85,7 @@ class Program
     static void Main(string[] args)
     {
         // Test the classes
-        Scripture scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        Scripture scripture = new Scripture("Helaman 5:12", "And now, my sons, remember, remember that it is upon the rock of our Redeemer, who is Christ, the Son of God, that ye must build your foundation; that when the devil shall send forth his mighty winds, yea, his shafts in the whirlwind, yea, when all his hail and his mighty storm shall beat upon you, it shall have no power over you to drag you down to the gulf of misery and endless wo, because of the rock upon which ye are built, which is a sure foundation, a foundation whereon if men build they cannot fall.");
 
         Console.WriteLine($"Scripture Reference: {scripture.GetReference()}");
         Console.WriteLine($"Scripture Text: {scripture.GetScriptureText()}");
